3f48244f88d2337c84f5f3e8b83850ae7febaedc
milla56 <lubomila14@gmail.com>
2023-04-21 19:45:38 +0100

    questions and answers were added

--- javascript/questions.js
+++ javascript/questions.js
@@ -2,27 +2,27 @@ var questions = [
   
     {
       question: "Commonly used data types Do Not include",
-      answer: ["strings", "booleans", "alerts", "numbers"],
-      correctAnswer: 'booleans', 
+      answer: ["1.Strings", "2.Booleans", "3.Alerts", "4.Numbers"],
+      correctAnswer: '3.Alerts', 
     },
     {
       question: "The condition in an if/else statement is enclosed with _____.",
-      answer: ["quotes", "curly brackets", "parenthesis", "square brackets"],
-      correctAnswer: "curly brackets",
+      answer: ["1.Quotes", "2.Curly brackets", "3.Parenthesis", "4.Square brackets"],
+      correctAnswer: "2.Curly brackets",
     },
     {
       question: "Arrays in JavaScript can be used to store _________.",
-      answer: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-      correctAnswer: "all of the above",
+      answer: ["1.Numbers and strings", "2.Other arrays", "3.Booleans", "4.All of the above"],
+      correctAnswer: "4.All of the above",
     },
     {
       question: "String values must be enclosed within____ when being assigned to variables",
-      answer: ["commas", "curly brackets", "quotes", "parenthesis"],
-      correctAnswer: "quotes",
+      answer: ["1.Commas", "2.Curly brackets", "3.Quotes", "4.Parenthesis"],
+      correctAnswer: "3.Quotes",
     },
     {
       question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-      answer: ["JavaScript", "teminal/bash", "for loops", "console.log"],
-      correctAnswer: "terminal/bash",
+      answer: ["1.JavaScript", "2.Teminal/bash", "3.For loops", "4.Console.log"],
+      correctAnswer: "2.Terminal/bash",
     },
   ];
--- javascript/script.js
+++ javascript/script.js
@@ -4,11 +4,15 @@ var questions = document.querySelector("#questions");
 var choices = document.querySelector('#choices');
 var userInitial = document.querySelector('#initial');
 var submit = document.querySelector('#submit');
-var feedback = document.querySelector('#feedback');
+var feedbackUser = document.querySelector('#feedback');
 
 let currentQuestion = 0;
 let remainingTime = questions.lenght*15;
 let timeInterval;
+
+// Set the sound 
+let winSound = new Audio('correct.wav');
+winSound();
 
 // The startQuiz function 
 
@@ -26,4 +30,48 @@ function showQuestion(){
   const questionTitle = document.querySelector('#question-title');
   questionTitle.textContent = questions[currentQuestion].question;
   choices.innerHTML = '';
+
+  for (var i=0;i< questions[currentQuestion].question; i++){
+    const button = document.createElement('button');
+    button.textContent = questions[currentQuestion].answer[i];
+    button.addEventListener('click',checkAnswer);
+    choices.appendChild('button');
+  }
 }
+
+// Check if andswer is correct
+function checkAnswer(event) {
+  event.preventDefault();
+  const clickedAnswer = event.target.textContent;
+  if(clickedAnswer === questions[currentQuestion].correctAnswer){
+      currentQuestion++;
+      if(currentQuestion === questions.length){
+          endQuiz();
+      } else {
+          showQuestion();
+      }
+      feedbackUser.textContent = 'Correct ';
+  } else {
+      remainingTime -= 10;
+      updateTime();
+      feedbackUser.textContent = 'Incorrect '
+  }
+  feedbackUser.removeAttribute('class', 'hide');
+  setTimeout(function(){
+      feedbackUser.setAttribute('class', 'hide');
+  }, 500);
+}
+
+ // correct answer + move to the next question
+     // if all questions are correct
+     //substract 10 seconds from the timer
+
+      //remove the class of hide to show feedback
+      // set time for how long to display feedback
+
+      // update the time remaining
+
+      // End of the quiz
+
+
+      //alert and sound parenthis 5 questions
